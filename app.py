7c80d4205c890b8f49877e4feb65db7d9e7314ba
Lavanya Chityal <lavanyachityal525@gmail.com>
2026-06-15 15:38:30 +0530

    Add register login and dashboard routes

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import mysql.connector
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+
+)
+print("MySQL Connected Successfully!")
+
+# Home Route
 @app.route('/')
 def home():
-    return "AI Interview Preparation System"
+    return render_template('index.html')
+
+# Register Route
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+# Login Route
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Dashboard Route
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
